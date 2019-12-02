db9f8169d3fdc8157a70b937b24a4c4e89a0caa0
Imran109 <cold3018@gmail.com>
2019-12-02 21:38:48 +0530

    Components Part1 reactjs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
+import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">
+              RestoRente Con Fusion
+            </NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
     </div>
   );
 }
