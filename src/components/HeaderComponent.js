16329ffb1713e22dd9656c3a5c44f29e84728e4e
Imran109 <cold3018@gmail.com>
2019-12-08 10:43:55 +0530

    Intro to Redux

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Navbar, NavbarBrand,Nav,NavbarToggler,Collapse,NavItem, Jumbotron} from 'reactstrap';
+import {Navbar, NavbarBrand,Nav,NavbarToggler,Collapse,NavItem, Jumbotron,Button, Modal, ModalHeader, ModalBody, Form, FormGroup , FormText , FormFeedback, Input, Label } from 'reactstrap';
 import {NavLink} from 'react-router-dom';
 
 class Header extends Component{
@@ -7,15 +7,29 @@ class Header extends Component{
     constructor(props){
          super(props);
          this.state = {
-             isNavOpen: false
+             isNavOpen: false,
+             isModalOpen : false,
+             
          };
-         this.toggleNav = this.toggleNav.bind(this);
+         this.toggleNav = this.toggleNav.bind(this);    
+         this.toggleModal = this.toggleModal.bind(this);
+         this.handleLogin = this.handleLogin.bind(this);
+    }
+    toggleModal(){
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
     }
 
+    handleLogin(event){
+        this.toggleModal();
+        alert("Username: "+this.username.value  + " Password: "+ this.password.value + " Remember: " + this.remember.checked);
+        event.preventDefault();
+    }
     toggleNav(){
         this.setState({
             isNavOpen: !this.state.isNavOpen
-        })
+        });
     }
     render(){
         return(
@@ -49,6 +63,13 @@ class Header extends Component{
                         </NavLink>
                     </NavItem>
                 </Nav>
+                <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <Button outline onClick={this.toggleModal}>
+                            <span className="fa fa-sign-in fa-lg"></span>Login
+                        </Button>
+                    </NavItem>
+                </Nav>
             </Collapse>
           </div>
         </Navbar>
@@ -67,6 +88,33 @@ class Header extends Component{
                 </div>
             </div>
         </Jumbotron>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+            <ModalHeader  toggle={this.toggleModal}>
+                Login
+            </ModalHeader>
+            <ModalBody>
+                <Form onSubmit={this.handleLogin}>
+                    <FormGroup>
+                        <Label htmlFor="username">
+                            Username
+                        </Label>
+                        <Input type="text" id="username" name="username" innerRef={(input)=>this.username=input} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor="password">
+                            Password
+                        </Label>
+                        <Input type="password" id="password" name="password" innerRef={(input)=>this.password=input}/>
+                    </FormGroup>
+                    <FormGroup check>
+                        <Label check>
+                            <Input type="checkbox" name="remeber" innerRef={(input)=>this.remember=input} />Remember me!
+                        </Label>
+                    </FormGroup>
+                    <Button type="submit" value="submit" color="primary">Login</Button>
+                </Form>
+            </ModalBody>
+        </Modal>
             </>
         )
     }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,37 +6,37 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { PROMOTIONS } from '../shared/promotions';
-import { LEADERS } from '../shared/leaders';
+import { Switch, Route, Redirect,withRouter } from 'react-router-dom';
+import {connect} from 'react-redux';
 
-
+const mapStateToProps = state=>{
+        return {
+            dishes: state.dishes,
+            comments: state.comments,
+            promotions: state.promotions,
+            leaders: state.leaders
+        }
+}
 class Main extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            dishes: DISHES,
-            comments: COMMENTS,
-            promotions: PROMOTIONS,
-            leaders: LEADERS
-        };
     }
+
+ 
     render() {
         const HomePage = () => {
             return (
-                <Home dish={this.state.dishes.filter(dish => dish.featured)[0]}
-                    promotion={this.state.promotions.filter(promo => promo.featured)[0]}
-                    leader={this.state.leaders.filter(leader => leader.featured)[0]}
+                <Home dish={this.props.dishes.filter(dish => dish.featured)[0]}
+                    promotion={this.props.promotions.filter(promo => promo.featured)[0]}
+                    leader={this.props.leaders.filter(leader => leader.featured)[0]}
                 />
             );
         }
         const DishWithId = ({match}) => {
             return (
                 <DishDetail 
-                dish={this.state.dishes.filter(dish => dish.id === parseInt(match.params.dishId, 10))[0]} 
-                comments={this.state.comments.filter(comment => comment.dishId === parseInt(match.params.dishId, 10))} 
+                dish={this.props.dishes.filter(dish => dish.id === parseInt(match.params.dishId, 10))[0]} 
+                comments={this.props.comments.filter(comment => comment.dishId === parseInt(match.params.dishId, 10))} 
                     />
             );
         }
@@ -45,10 +45,10 @@ class Main extends Component {
                 <Header />
                 <Switch>
                     <Route path="/home" component={HomePage} />
-                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />} />
                     <Route path="/menu/:dishId" component={DishWithId} />
                     <Route exact path="/contactus" component={Contact} />
-                    <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
+                    <Route exact path="/aboutus" component={() => <About leaders={this.props.leaders} />} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
@@ -56,4 +56,4 @@ class Main extends Component {
         );
     }
 }
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
